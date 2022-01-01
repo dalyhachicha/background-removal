5fbe62d2a28a3ea5f0c83414a26c21114b69c565
dalyhachicha <dalyhachicha7@gmail.com>
2022-01-01 20:36:23 +0100

    Update bg-removal.py

--- bg-removal.py
+++ bg-removal.py
@@ -1,17 +1,27 @@
 import cv2
 import cvzone
+
 from cvzone.SelfiSegmentationModule import SelfiSegmentation
 
 cap = cv2.VideoCapture(0)
 cap.set(3,640) #Width
 cap.set(4,480) #Height
 
-segmentor = SelfiSegmentation() #model1 (faster) par default
+#Background image path
+bg_img_path = r"C:\\Users\\Daly\\Desktop\\Background removal\\source code\\bg-imgs\\1.jpg" 
+
+img_bg = cv2.imread(bg_img_path)
+
+segmentor = SelfiSegmentation() #model1 (faster/landscape) DEFAULT
 
 
 while True:
     success, img = cap.read()
-    img_bg = segmentor.removeBG(img, (255,0,255),threshold=0.8)
-    cv2.imshow("Video Feed", img)
-    cv2.imshow("Background removed", img_bg)
-    cv2.waitKey(1)
+    img2 = segmentor.removeBG(img, imgBg=img_bg,threshold=0.8)
+    img_stack = cvzone.stackImages([img,img2],2,1)
+    cv2.imshow("Video Background removal", img_stack)
+
+
+    k = cv2.waitKey(33)
+    if k==27:  # Esc key to stop
+        break
